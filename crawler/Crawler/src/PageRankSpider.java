a235167f228602d9cd84122ad5642700fb6d615d
Pawe³ Rych³y <pawelrychly@gmail.com>
2013-12-14 15:27:40 +0100

    Init project

---
+++ crawler/Crawler/src/PageRankSpider.java
@@ -0,0 +1,17 @@
+
+import ir.webutils.Spider;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Paweł
+ */
+public class PageRankSpider extends Spider {
+    public static void main(){
+        
+    }
+}
