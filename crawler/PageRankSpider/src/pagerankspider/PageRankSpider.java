98f4d9483cc5ec5584d32bf26cc9531196e27da7
Pawe³ Rych³y <pawelrychly@gmail.com>
2013-12-14 16:31:15 +0100

    Poprawiony folder

---
+++ crawler/PageRankSpider/src/pagerankspider/PageRankSpider.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pagerankspider;
+
+import ir.webutils.Spider;
+
+/**
+ *
+ * @author Paweł
+ */
+public class PageRankSpider extends Spider {
+
+  
+    
+}
